b3fa796ca0a37cb5fd0a83168bd9402f6cf54f60
Smety2001 <tomas.smetana@students.plymouth.ac.uk>
2024-06-04 15:27:32 +0200

    merged install and start batch files

--- start.bat
+++ start.bat
@@ -1,5 +1,40 @@
 @echo off
 cd "%~dp0"
-curl -f -o main.py https://raw.githubusercontent.com/Smety2001/Stellaria-Advanced-Launcher/main/main.py
-pip install -r https://raw.githubusercontent.com/Smety2001/Stellaria-Advanced-Launcher/main/requirements.txt
-start /MIN pythonw main.py
+
+echo Checking if python is installed
+where python >nul 2>&1
+if %errorlevel%==0 (
+    echo Python is already installed
+) else (
+    echo Installing python 3.12.3...
+    PowerShell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe' -OutFile 'python-3.12.3-amd64.exe'"
+    start /wait python-3.12.3-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+    del python-3.12.3-amd64.exe
+    echo Python 3.12.3 installed
+)
+echo Checking dependencies...
+powershell -Command ^
+  "$apiUrl = 'https://api.github.com/repos/Smety2001/Stellaria-Advanced-Launcher/contents/requirements.txt?ref=main';" ^
+  "$timestamp = Get-Date -Format 'yyyyMMddHHmmss';$uniqueUrl = $apiUrl + '&timestamp=' + $timestamp;" ^
+  "$headers = @{ 'Cache-Control' = 'no-cache' };" ^
+  "$response = Invoke-RestMethod -Uri $uniqueUrl -Headers $headers;" ^
+  "$content = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($response.content));" ^
+  "$lib = $content -split '\n' | Where-Object { $_ -ne '' } | ForEach-Object { $_.Trim() };" ^
+  "& pip install $lib"
+echo Dependencies checked
+
+echo Updating stellaria advanced launcher...
+powershell -Command ^
+  "$apiUrl = 'https://api.github.com/repos/Smety2001/Stellaria-Advanced-Launcher/contents/main.py?ref=main';" ^
+  "$timestamp = Get-Date -Format 'yyyyMMddHHmmss';" ^
+  "$uniqueUrl = $apiUrl + '&timestamp=' + $timestamp;" ^
+  "$headers = @{ 'Cache-Control' = 'no-cache' };" ^
+  "$response = Invoke-RestMethod -Uri $uniqueUrl -Headers $headers;" ^
+  "$content = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($response.content));" ^
+  "$filePath = '%~dp0' + $response.name;" ^
+  "Set-Content -Path $filePath -Value $content -Encoding utf8"
+echo Stellaria advanced launcher updated
+
+echo Starting stellaria advanced launcher...
+start /min python main.py
+echo Stellaria advanced launcher started
